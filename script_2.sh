40e37c4a71de3a48a68d6ae13de6c658fe8954ec
jembrown <jeremymbrown@gmail.com>
2018-12-08 12:29:19 -0600

    Updating week 6 comments

--- script_2.sh
+++ script_2.sh
@@ -6,5 +6,4 @@ echo "Enter name of file that has numbers separated by spaces: "
 read fileName
 cat $fileName | ./script_1.sh
 
-# DB: Good idea, but this will only work if script_1.sh accepts numbers as command-line 
-#     arguments and you structured it like this: ./script_1.sh `cat $fileName`.
+# DB: Good!
